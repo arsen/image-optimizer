eef91b5e019c58f34a222a9b044a66c819a9c795
Arsen Ghazaryan <arsenghazaryan@c02nt0u9g3qd.corp.aofl.com>
2016-05-31 18:11:41 -0700

    Update version number and clean up files

--- test.js
+++ test.js
@@ -1,5 +1,5 @@
-var glob = require("glob");
+//var glob = require("glob");
 
-glob('/Users/arsenghazaryan/Documents/aofl/member.abcmouse.com/abcmouse_classroom/first_grade_com_en/img/april.png', function(err, files) {
-	console.log(files);
-});
+//glob('', function(err, files) {
+//	console.log(files);
+//});
