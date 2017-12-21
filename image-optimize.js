578af8d01e34330b14eaf5ebaedc48bd4898f782
Allan.Calix <allan.calix@aofl.com>
2017-12-20 18:05:49 -0800

    Refactor to break up functionality to smaller functions

--- image-optimize.js
+++ image-optimize.js
@@ -19,13 +19,14 @@ program
   .version('0.0.1')
   .option('-t, --type [IMGTYPE]', 'File type (for now only .PNG is supported) [PNG]', 'png')
   .option('-p, --path [PATH]', 'Path to image file or folder', '')
+  .option('-a, --audit', 'Check which files will be optimized', '')
+  .option('-v, --verbose', 'Make some noise', '')
   .parse(process.argv);
 
 if (typeof fileTypes[program.type] === 'undefined') {
 	console.log('Invalid FILE TYPE');
 	process.exit();
 }
-
 
 try {
 	program.path = path.normalize(program.path);
@@ -48,13 +49,17 @@ catch(e) {
 	process.exit();
 }
 
-
-
 var currentFileIndex, dirData;
 
 var origSize, optimizedSize;
 
-utils.getSizeInfo(imgDir + fileType, function(err, result) {
+utils.getSizeInfo(imgDir + fileType, (err, result) => {
+  if (err) { throw err }
+
+  optimizeBatch(result);
+});
+
+async function optimizeBatch(result) {
 	dirData = result;
 	console.log('### Before optimizing ###');
 	console.log('Files: ' + result.files.length);
@@ -66,65 +71,86 @@ utils.getSizeInfo(imgDir + fileType, function(err, result) {
 		var dir = tmpDir + path.dirname(result.files[i]);
 		fsExtra.mkdirsSync(dir);
 	}
-	
-	currentFileIndex = 0;
-	resursiveOptimize(result.files[currentFileIndex], tmpDir + result.files[currentFileIndex]);
-});
+
+  for (let i = 0, j = dirData.files.length; i < j; i ++) {
+    let file = dirData.files[i];
+    process.stdout.write(i + ". Processing file: " + dirData.files[i]+' ... ');
+    try {
+      let fileOutput = await optimize(file, tmpDir + file);
+      if (fileOutput.changePercent >= 10) {
+        fileChange.push(fileOutput.src);
+      }
+      console.log('done! ('+fileOutput.changePercent+'%)');
+    } catch (e) {
+      console.log(e);
+    }
+  }
+
+  utils.getSizeInfo(tmpDir + imgDir + fileType, function(err, result) {
+    optimizedSize = result.size;
+
+    console.log('### After optimizing ###');
+    console.log('Files: ' + result.files.length);
+    console.log('Total Size: ' + Math.round(result.size / 1024) + 'kb');
+    var totalSizeReduced = Math.round((origSize - optimizedSize) / 1024);
+    var totalSizeReducedPercent = 100 - Math.round(optimizedSize / origSize * 100);
+    console.log('Total size reduced by: '+totalSizeReduced+'kb ('+totalSizeReducedPercent+'%)');
+    console.log('#########################');
+
+    if (program.audit) {
+      cleanUpTempFolder();
+      console.log("\nFile Changes Greater than 5%");
+      printFiles(fileChange);
+    } else {
+      var copyOptions = {
+        clobber: true
+      };
+      fsExtra.copy(tmpDir + imgDir, imgDir, copyOptions, function(err) {
+        if (err) {
+          console.log('Error copying files back to original location');
+        } else {
+          console.log('Done copying files back to original location');
+          cleanUpTempFolder();
+        }
+      });
+    }
+  });
+}
 
 function cleanUpTempFolder(){
   fsExtra.remove(tmpDir, function(err) {
     if (err) {
       console.log('error cleaning tmp files');
     }
-    else {
-      console.log('Done cleaning TMP files');
-    }
     process.exit();
   });
 }
 
-var currentFileIndex = 0;
-var resursiveOptimize = function(src, dest) {
-	process.stdout.write(currentFileIndex + ". Processing file: " + src+' ... ');
-	execFile(pngquant, ['-o', dest, src], function(err) {
-		if (err) {
-			console.log(err);
-			cleanUpTempFolder();
-		}
-		var sizeBefore = Math.round(fs.statSync(src).size / 1024);
-		var sizeAfter = Math.round(fs.statSync(dest).size / 1024);
-		var changePercent = 100 - Math.round(sizeAfter / sizeBefore * 100);
-		changePercent = Math.max(0, changePercent);
-		console.log('done! ('+changePercent+'%)');
-		currentFileIndex++;
-		if (currentFileIndex < dirData.files.length) {
-			resursiveOptimize(dirData.files[currentFileIndex], tmpDir + dirData.files[currentFileIndex]);
-		} else {
-			utils.getSizeInfo(tmpDir + imgDir + fileType, function(err, result) {
-				optimizedSize = result.size;
+let fileChange = [];
 
-				console.log('### After optimizing ###');
-				console.log('Files: ' + result.files.length);
-				console.log('Total Size: ' + Math.round(result.size / 1024) + 'kb');
-				var totalSizeReduced = Math.round((origSize - optimizedSize) / 1024);
-				var totalSizeReducedPercent = 100 - Math.round(optimizedSize / origSize * 100);
-				console.log('Total size reduced by: '+totalSizeReduced+'kb ('+totalSizeReducedPercent+'%)');
-				console.log('#########################');
-				
+function optimize(src, dest) {
+  return new Promise((resolve, reject) => {
+    execFile(pngquant, ['-o', dest, src], function(err) {
+      if (err) {
+        cleanUpTempFolder();
+        reject(err);
+      }
+      var sizeBefore = Math.round(fs.statSync(src).size / 1024);
+      var sizeAfter = Math.round(fs.statSync(dest).size / 1024);
+      var changePercent = 100 - Math.round(sizeAfter / sizeBefore * 100);
+      changePercent = Math.max(0, changePercent);
 
+      resolve({
+        src,
+        dest,
+        changePercent
+      });
+    });
+  });
+}
 
-				var copyOptions = {
-					clobber: true
-				};
-				fsExtra.copy(tmpDir + imgDir, imgDir, copyOptions, function(err) {
-					if (err) {
-						console.log('Error copying files back to original location');
-					} else {
-						console.log('Done copying files back to original location');
-						cleanUpTempFolder();
-					}
-				});
-			});
-		}
-	});
-};
+function printFiles(files) {
+  for (let i = 0, j = files.length; i < j; i ++) {
+    process.stdout.write(files[i] + '\n');
+  }
+}
